963964adb03a646fe45da4a205b87bdcdee60fba
Marc d'Entremont <cramhead@gmail.com>
2014-08-07 16:20:10 -0700

    removal of race conditions in reader code

--- client/controllers.js
+++ client/controllers.js
@@ -118,33 +118,43 @@ Template.c_clientside_upload.rendered = function () {
         if (!file.type.match('image.*')) {
           continue;
         }
-        var fileName = file.name; // get the name of file to use as annotation
+
         var reader = new FileReader();
 
-        reader.onload = function () {
-
-
-          var pendingFile = {
-            file_name: fileName,
-            created_at: new Date(),
-            uploading: true
-          };
-          var db_id = _cloudinary.insert(pendingFile);
-
-          _cloudinary.update({
-            file_name: fileName
-          }, {
-            $set: {
+        reader.onload = (function (fileRead) {
+          var fileName = fileRead.name; // get the name of file to use as annotation
+          return function () {
+            var pendingFile = {
+              file_name: fileName,
+              created_at: new Date(),
+              uploading: true,
               base64data: this.result
-            }
-          }, function (err) {
-            if (err) {
-              App.logger.error("Error saving base64 data in _cloudinary",
-                err.reason);
-            }
-          });
+            };
+            console.log("insert with " + EJSON.stringify(pendingFile));
+            var db_id = _cloudinary.insert(pendingFile, function (err) {
+              if (err) {
+                App.logger.error(
+                  "Error saving on reader.onload to _cloudinary",
+                  err.reason);
+              }
+            });
+          };
 
-        };
+          // return function (evt) {
+          //   var base64 = this.result;
+          //   _cloudinary.update({file_name: fileName}, {$set: {base64data: base64}}, function (err) {
+          //     if (err) {
+          //       App.logger.error(
+          //         "Error saving base64 data in _cloudinary",
+          //         err.reason);
+          //     }else{
+          //       console.log("saved basedata for " + fileName + ": " + base64);
+          //     }
+          //   });
+          //
+          // };
+
+        }(file));
 
         reader.readAsDataURL(file);
       }
@@ -159,47 +169,54 @@ Template.c_clientside_upload.rendered = function () {
   input.unsigned_cloudinary_upload(preset, cloudinaryUploadParams).bind(
     'cloudinarydone', function (e, data) {
 
-      var numFiles = data.files.length;
-      for (var i = 0; i < numFiles; i++) {
+      var fileName = data.files[0].name;
+      var publicId = data.result.public_id;
+      console.log('done with file ' + fileName + " publicId " + publicId);
 
-        var result = data.result;
-        var fileName = data.files[i].name;
-        var record = _cloudinary.findOne({
-          file_name: fileName
-        });
+      // var numFiles = data.files.length;
+      // for (var i = 0; i < numFiles; i++) {
 
-        _.extend(result, {
-          file_name: fileName,
-          base64data: record.base64data,
-          total_uploaded: result.bytes,
-          percent_uploaded: 100,
-          uploading: false
-        });
+      var result = data.result;
+      console.log('searching for ' + fileName)
+      var record = _cloudinary.findOne({
+        file_name: fileName
+      });
 
-        // get the name of the image
-        var fileName = data.files[i].name;
-        var publicId = data.result.public_id;
-        console.log('file ' + fileName + " publicId " + publicId);
-
-        _cloudinary.update({
-          file_name: fileName,
-        }, result);
-
-        if (record.callback) {
-          Meteor.call(record.callback, result);
-        }
-
-        // var imageTransforms = {
-        //   format: 'jpg',
-        //   width: 150,
-        //   height: 100,
-        //   crop: 'thumb',
-        //   gravity: 'face',
-        //   effect: 'saturation:50'
-        // };
-
-        //  augmentPreview(fileName, publicId);
+      if (!record) {
+        console.log('did not find ' + fileName);
       }
+
+      _.extend(result, {
+        file_name: fileName,
+        total_uploaded: result.bytes,
+        percent_uploaded: 100,
+        uploading: false
+      });
+      if (record.base64data) {
+        result.base64data = record.base64data
+      }
+
+      // get the name of the image
+
+      _cloudinary.update({
+        file_name: fileName,
+      }, result);
+
+      if (record.callback) {
+        Meteor.call(record.callback, result);
+      }
+
+      // var imageTransforms = {
+      //   format: 'jpg',
+      //   width: 150,
+      //   height: 100,
+      //   crop: 'thumb',
+      //   gravity: 'face',
+      //   effect: 'saturation:50'
+      // };
+
+      //  augmentPreview(fileName, publicId);
+      // }
 
     }).bind('cloudinaryprogress', function (e, data) {
 
@@ -251,87 +268,87 @@ var getCloudinaryOptions = function ($input) {
   return options;
 };
 
-var augmentPreview = function (previewImageName, publicId, transforms) {
-  // if there are no transforms then just annotate, otherwise replace
-  if (typeof transforms === 'undefined') {
-    annotatePreview(previewImageName, publicId);
-  } else {
-    replacePreview(previewImageName, publicId, transforms);
-  }
-};
-
-var replacePreview = function (previewImageName, publicId, transforms) {
-  // create a cloudinaryImage
-  var cloudinaryImage = $.cloudinary.image(publicId, transforms);
-
-  //annotate it with the public_id
-  cloudinaryImage.attr("public_id", publicId);
-
-  // replace the preview with the cloudinary image
-  $("img[data-name='" + previewImageName + "']").replaceWith(
-    cloudinaryImage);
-};
-
-var annotatePreview = function (previewImageName, publicId) {
-  $("img[data-name='" + previewImageName + "']").attr("public_id", publicId);
-};
-
-function buildImagePreview(evt, tmpl, options) {
-  if (window.File && window.FileReader && window.FileList && window.Blob) {
-    var files = evt.target.files;
-    optons = options || {};
-
-    var file;
-    for (var i = 0; file = files[i]; i++) {
-      // if the file is not an image, continue
-      if (!file.type.match('image.*')) {
-        continue;
-      }
-
-      var reader = new FileReader();
-      reader.onload = (function (fileRead) {
-        var fileName = fileRead.name; // get the name of file to use as annotation
-        var pendingFile = {
-          file_name: fileName,
-          created_at: new Date(),
-          uploading: true
-        };
-        options.db_id = _cloudinary.insert(pendingFile);
-
-        return function (evt, options) {
-          var div = $('<div>');
-          // create a progress div
-          var progress = $('<div>');
-          progress.addClass("progress");
-          progress.attr('data-progress', fileName);
-
-          // append it to the container
-          div.append(progress);
-
-          _cloudinary.update({
-            file_name: fileName
-          }, {
-            $set: {
-              base64data: evt.target.result
-            }
-          }, function (err) {
-            if (err) {
-              App.logger.error("Error saving base64 data in _cloudinary",
-                err.reason);
-            }
-          })
-          // add the preview image
-          div.append('<img style="width: 90px;" src="' + evt.target.result +
-            '" data-name=' + fileName + ' />');
-
-          // add it to the thumbs
-          $('.thumbnails').append(div);
-        };
-      }(file, options));
-
-      reader.readAsDataURL(file);
-    }
-  } else {
-    alert('The File APIs are not fully supported in this browser.');
-  }
-};
+// var augmentPreview = function (previewImageName, publicId, transforms) {
+//   // if there are no transforms then just annotate, otherwise replace
+//   if (typeof transforms === 'undefined') {
+//     annotatePreview(previewImageName, publicId);
+//   } else {
+//     replacePreview(previewImageName, publicId, transforms);
+//   }
+// };
+//
+// var replacePreview = function (previewImageName, publicId, transforms) {
+//   // create a cloudinaryImage
+//   var cloudinaryImage = $.cloudinary.image(publicId, transforms);
+//
+//   //annotate it with the public_id
+//   cloudinaryImage.attr("public_id", publicId);
+//
+//   // replace the preview with the cloudinary image
+//   $("img[data-name='" + previewImageName + "']").replaceWith(
+//     cloudinaryImage);
+// };
+//
+// var annotatePreview = function (previewImageName, publicId) {
+//   $("img[data-name='" + previewImageName + "']").attr("public_id", publicId);
+// };
+//
+// function buildImagePreview(evt, tmpl, options) {
+//   if (window.File && window.FileReader && window.FileList && window.Blob) {
+//     var files = evt.target.files;
+//     optons = options || {};
+//
+//     var file;
+//     for (var i = 0; file = files[i]; i++) {
+//       // if the file is not an image, continue
+//       if (!file.type.match('image.*')) {
+//         continue;
+//       }
+//
+//       var reader = new FileReader();
+//       reader.onload = (function (fileRead) {
+//         var fileName = fileRead.name; // get the name of file to use as annotation
+//         var pendingFile = {
+//           file_name: fileName,
+//           created_at: new Date(),
+//           uploading: true
+//         };
+//         options.db_id = _cloudinary.insert(pendingFile);
+//
+//         return function (evt, options) {
+//           var div = $('<div>');
+//           // create a progress div
+//           var progress = $('<div>');
+//           progress.addClass("progress");
+//           progress.attr('data-progress', fileName);
+//
+//           // append it to the container
+//           div.append(progress);
+//
+//           _cloudinary.update({
+//             file_name: fileName
+//           }, {
+//             $set: {
+//               base64data: evt.target.result
+//             }
+//           }, function (err) {
+//             if (err) {
+//               App.logger.error("Error saving base64 data in _cloudinary",
+//                 err.reason);
+//             }
+//           })
+//           // add the preview image
+//           div.append('<img style="width: 90px;" src="' + evt.target.result +
+//             '" data-name=' + fileName + ' />');
+//
+//           // add it to the thumbs
+//           $('.thumbnails').append(div);
+//         };
+//       }(file, options));
+//
+//       reader.readAsDataURL(file);
+//     }
+//   } else {
+//     alert('The File APIs are not fully supported in this browser.');
+//   }
+// };
